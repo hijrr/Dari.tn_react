dad41f15049c6326ff2115c639c9ad1011f73436
achwak <belliliachwak@gmail.com>
2025-10-14 19:57:22 +0100

    api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
-import './App.css';
-import Page1 from './components/Page1';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [clients, setClients] = useState([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/clients")
+      .then(res => setClients(res.data))
+      .catch(err => setError(err.message));
+  }, []);
+
   return (
-    <div className="App">
-      <Page1 />
+    <div>
+      <h1>Liste des clients</h1>
+      {error && <p style={{ color: "red" }}>Erreur : {error}</p>}
+      <ul>
+        {clients.map(client => (
+          <li key={client.id}>{client.nom} - {client.email}</li>
+        ))}
+      </ul>
     </div>
   );
 }
